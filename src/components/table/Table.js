a6bd3f3e8139405b58ecf1e94ce9fac66197b7bf
sa1amander83 <virom8@yandex.ru>
2021-04-10 20:14:59 +0300

    added resize code

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,7 +3,30 @@ import {createTable} from '@/components/table/table.template'
 
 export class Table extends ExcelComponent {
   static className ='excel__table'
+
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    });
+  }
+
+
   toHTML() {
     return createTable()
+  }
+
+  onClick() {
+    console.log('click')
+  }
+
+  onMousedown(event) {
+    console.log('mousedown', event.target)
+  }
+
+  onMousemove() {
+    console.log('mousemove')
+  }
+  onMouseup() {
+    console.log('mouseup')
   }
 }
